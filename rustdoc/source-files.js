0d48175bc054512a51f7818111739703b0bb08d2
hanabi1224 <harlowmoo@gmail.com>
2022-11-16 12:00:03 +0000

    Deploying to gh-pages from @ ChainSafe/forest@e5de79af467d7c9478d357c7618d3aa862dbdb24 🚀

--- rustdoc/source-files.js
+++ rustdoc/source-files.js
@@ -4,7 +4,7 @@ var sourcesIndex = JSON.parse('{\
 "forest_auth":["",[],["lib.rs"]],\
 "forest_beacon":["",[],["beacon_entries.rs","drand.rs","lib.rs","mock_beacon.rs"]],\
 "forest_blocks":["",[["header",[],["json.rs","mod.rs"]]],["block.rs","election_proof.rs","errors.rs","gossip_block.rs","lib.rs","ticket.rs","tipset.rs"]],\
-"forest_chain":["",[["store",[],["base_fee.rs","chain_store.rs","errors.rs","index.rs","mod.rs","tipset_tracker.rs"]]],["lib.rs","weight.rs"]],\
+"forest_chain":["",[["store",[],["base_fee.rs","chain_store.rs","errors.rs","index.rs","metrics.rs","mod.rs","tipset_tracker.rs"]]],["lib.rs","weight.rs"]],\
 "forest_chain_sync":["",[],["bad_block_cache.rs","chain_muxer.rs","consensus.rs","lib.rs","metrics.rs","network_context.rs","peer_manager.rs","sync_state.rs","tipset_syncer.rs","validation.rs"]],\
 "forest_cli":["",[["cli",[],["auth_cmd.rs","chain_cmd.rs","config_cmd.rs","db_cmd.rs","fetch_params_cmd.rs","genesis_cmd.rs","mod.rs","mpool_cmd.rs","net_cmd.rs","send_cmd.rs","snapshot_cmd.rs","state_cmd.rs","sync_cmd.rs","wallet_cmd.rs"]]],["lib.rs"]],\
 "forest_cli_shared":["",[["cli",[],["client.rs","config.rs","mod.rs","snapshot_fetch.rs"]],["logger",[],["mod.rs"]]],["lib.rs"]],\
