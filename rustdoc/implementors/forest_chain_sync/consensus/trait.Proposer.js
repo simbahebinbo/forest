8fcaa542b88eaffc388666f2422c4da638eb6179
lemmih <lemmih@gmail.com>
2022-11-29 13:30:18 +0000

    Deploying to gh-pages from @ ChainSafe/forest@73abc5747930e1f0af182cfe9e2c25f31b69d58a 🚀

--- rustdoc/implementors/forest_chain_sync/consensus/trait.Proposer.js
+++ rustdoc/implementors/forest_chain_sync/consensus/trait.Proposer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"forest_deleg_cns":[["impl <a class=\"trait\" href=\"forest_chain_sync/consensus/trait.Proposer.html\" title=\"trait forest_chain_sync::consensus::Proposer\">Proposer</a> for <a class=\"struct\" href=\"forest_deleg_cns/struct.DelegatedProposer.html\" title=\"struct forest_deleg_cns::DelegatedProposer\">DelegatedProposer</a>"]]
+"forest_deleg_cns":[["impl Proposer for <a class=\"struct\" href=\"forest_deleg_cns/struct.DelegatedProposer.html\" title=\"struct forest_deleg_cns::DelegatedProposer\">DelegatedProposer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
