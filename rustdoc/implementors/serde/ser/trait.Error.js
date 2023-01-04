792f914a36534fa4339eeff3232f3d4f214e1587
LesnyRumcajs <lesny.rumcajs@gmail.com>
2023-01-04 15:38:17 +0000

    Deploying to gh-pages from @ ChainSafe/forest@9df73049c878cd828131fcc6c8206bdd5ca62bd9 🚀

--- rustdoc/implementors/serde/ser/trait.Error.js
+++ rustdoc/implementors/serde/ser/trait.Error.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "forest_encoding":[],
-"forest_ipld":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.150/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"forest_ipld/enum.Error.html\" title=\"enum forest_ipld::Error\">Error</a>"]]
+"forest_ipld":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.152/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"forest_ipld/enum.Error.html\" title=\"enum forest_ipld::Error\">Error</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
