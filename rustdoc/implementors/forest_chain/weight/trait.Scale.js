48c7bbb73d2849291391171a83ba0f71d6b72695
LesnyRumcajs <lesny.rumcajs@gmail.com>
2023-01-09 13:21:03 +0000

    Deploying to gh-pages from @ ChainSafe/forest@b4ab951570542e26468630db88ad6f5a173ca463 🚀

--- rustdoc/implementors/forest_chain/weight/trait.Scale.js
+++ rustdoc/implementors/forest_chain/weight/trait.Scale.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"forest_deleg_cns":[["impl <a class=\"trait\" href=\"forest_chain/weight/trait.Scale.html\" title=\"trait forest_chain::weight::Scale\">Scale</a> for <a class=\"struct\" href=\"forest_deleg_cns/struct.DelegatedConsensus.html\" title=\"struct forest_deleg_cns::DelegatedConsensus\">DelegatedConsensus</a>"]],
-"forest_fil_cns":[["impl&lt;B&gt; <a class=\"trait\" href=\"forest_chain/weight/trait.Scale.html\" title=\"trait forest_chain::weight::Scale\">Scale</a> for <a class=\"struct\" href=\"forest_fil_cns/struct.FilecoinConsensus.html\" title=\"struct forest_fil_cns::FilecoinConsensus\">FilecoinConsensus</a>&lt;B&gt;"]]
+"forest_deleg_cns":[["impl Scale for <a class=\"struct\" href=\"forest_deleg_cns/struct.DelegatedConsensus.html\" title=\"struct forest_deleg_cns::DelegatedConsensus\">DelegatedConsensus</a>"]],
+"forest_fil_cns":[["impl&lt;B&gt; Scale for <a class=\"struct\" href=\"forest_fil_cns/struct.FilecoinConsensus.html\" title=\"struct forest_fil_cns::FilecoinConsensus\">FilecoinConsensus</a>&lt;B&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
