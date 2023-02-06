973de7a76a5c196c2a424d4983932697ef04f89c
elmattic <elmattic@users.noreply.github.com>
2023-02-06 11:43:31 +0000

    Deploying to gh-pages from @ ChainSafe/forest@25d0438a031cee97b192ef976df46732e4fb1f7a 🚀

---
+++ rustdoc/forest_libp2p_bitswap/task/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["sleep","Waits until `duration` has elapsed."],["spawn","Spawns a new asynchronous task, returning a `JoinHandle` for it."],["spawn_blocking","Runs the provided closure on a thread where blocking is acceptable."],["timeout","Requires a `Future` to complete before the specified duration has elapsed."]]};
--- rustdoc/implementors/libp2p_swarm/behaviour/trait.NetworkBehaviour.js
+++ rustdoc/implementors/libp2p_swarm/behaviour/trait.NetworkBehaviour.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "forest_libp2p":[["impl NetworkBehaviour for <a class=\"struct\" href=\"forest_libp2p/hello/struct.HelloBehaviour.html\" title=\"struct forest_libp2p::hello::HelloBehaviour\">HelloBehaviour</a>"]],
-"forest_libp2p_bitswap":[["impl NetworkBehaviour for <a class=\"struct\" href=\"forest_libp2p_bitswap/struct.BitswapBehaviour.html\" title=\"struct forest_libp2p_bitswap::BitswapBehaviour\">BitswapBehaviour</a><span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;RequestResponse&lt;BitswapRequestResponseCodec&gt;: NetworkBehaviour,</span>"]]
+"forest_libp2p_bitswap":[["impl NetworkBehaviour for <a class=\"struct\" href=\"forest_libp2p_bitswap/struct.BitswapBehaviour.html\" title=\"struct forest_libp2p_bitswap::BitswapBehaviour\">BitswapBehaviour</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
