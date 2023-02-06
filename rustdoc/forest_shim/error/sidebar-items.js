e57f10a51520b20d77187afcac1ef7300c46d9a1
LesnyRumcajs <lesny.rumcajs@gmail.com>
2023-02-06 16:37:59 +0000

    Deploying to gh-pages from @ ChainSafe/forest@65fae9307ce5eba44065bf8d9c01bc5bb967cd87 🚀

---
+++ rustdoc/forest_shim/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["ExitCode","Newtype wrapper for FVM `ExitCode`."]]};
--- rustdoc/forest_shim/sidebar-items.js
+++ rustdoc/forest_shim/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["address",""],["bigint",""],["econ",""],["version",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["address",""],["bigint",""],["econ",""],["error",""],["version",""]],"trait":[["Inner","Helper trait to re-use static methods and constants. The usage is awkward but it avoids code duplication."]]};
